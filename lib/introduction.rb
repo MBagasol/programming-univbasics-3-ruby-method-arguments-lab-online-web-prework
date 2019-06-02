9869696cb15e431b9e2e7fa5d160273008f8aa48
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-02 00:19:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,5 +11,5 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name)
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
